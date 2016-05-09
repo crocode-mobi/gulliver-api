91f49f373e0bb16f1a44aba3eb34ec28664abed3
Roberto Marco Sánchez <robmarco@gmail.com>
2016-05-09 14:24:29 +0200

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.assets.enabled = false
+
   # Configuration Paperclip with AWS S3
   config.paperclip_defaults = {
     :storage => :s3,
